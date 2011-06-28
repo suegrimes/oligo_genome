fd8d9be6a509a063dfe66ec0c61f0e25088bc65d
Sue Grimes <sgrimes@stanford.edu>
2011-06-27 21:25:01 -0700

    Modifications for chromosome .gz downloads

--- app/controllers/downloads_controller.rb
+++ app/controllers/downloads_controller.rb
@@ -1,5 +1,6 @@
 class DownloadsController < ApplicationController
   FILE_PATH = File.join(RAILS_ROOT, "public/files")
+  FILE_NAME = 'oligo_design_v18_chr@.txt.gz'
   CHROMOSOMES = %w{1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 X Y}
   
   def index
@@ -11,12 +12,11 @@ class DownloadsController < ApplicationController
   #*******************************************************************************************#
   def zip_save
     params[:chr_num] ||= 1
-    #file_name = 'oligo_design_v18_inf_flank_c_1000_24KmerMask_thresh1.0.chr1.reformatted.txt.gz'
-    #file_name.gsub!(/@/, params[:chr_num].to_s)
-    file_name = 'Selector_Stats_06072011.txt'
+    file_name = FILE_NAME.gsub(/@/, params[:chr_num].to_s)
     filepath = File.join(FILE_PATH, file_name)
 
     if FileTest.file?(filepath)
+      
       flash[:notice] = "Zip file #{filepath} successfully downloaded"
       #send_file(filepath, :disposition => "attachment")
       redirect_to :action => :index

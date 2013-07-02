2089eba4f65e7f6f72cfde3dcdf69695a7aa82f0
Jennifer Palm <jpalm@stanford.edu>
2013-07-02 13:40:00 -0700

    changed path to assets

--- config/initializers/app_params.rb
+++ config/initializers/app_params.rb
@@ -6,4 +6,5 @@ CAPISTRANO_DEPLOY = "#{Rails.root}".include?('releases')
 ZIP_REL_PATH = (CAPISTRANO_DEPLOY ? File.join("..", "..", "shared", "files") : File.join("public", "files"))
 ZIP_ABS_PATH = File.join("#{Rails.root}", ZIP_REL_PATH)
 
-BED_ABS_PATH = File.join("#{Rails.root}", "public", "bed_files")
+#BED_ABS_PATH = File.join("#{Rails.root}", "public", "bed_files")
+BED_ABS_PATH = File.join("#{Rails.root}", "app/assets/", "bed_files")
